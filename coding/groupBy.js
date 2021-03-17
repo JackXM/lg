5a9a5b8569cd49e933cf5b2e3adef89ed3716c1f
肖梦 <xiaom@nequal.com>
2021-03-17 22:26:58 +0800

    feat: reduce forEach实现groupBy

---
+++ coding/groupBy.js
@@ -0,0 +1,33 @@
+// reduce实现groupBy
+const people = [
+  { name: "xiao", age: 20 },
+  { name: "meng", age: 21 },
+  { name: "wang", age: 21 },
+  { name: "yi", age: 20 },
+];
+
+// function groupBy(arr, property) {
+//   return arr.reduce((data, item) => {
+//     const key = item[property];
+//     if (!data[key]) {
+//       data[key] = [];
+//     }
+//     data[key].push(item);
+//     return data;
+//   }, {});
+// }
+
+// 用forEach实现groupBy
+function groupBy(arr, property) {
+  const resp = {};
+  arr.forEach((item, index) => {
+    const key = item[property];
+    if (!resp[key]) {
+      resp[key] = [];
+    }
+    resp[key].push(item);
+  });
+  return resp;
+}
+
+console.log(groupBy(people, "age"));
